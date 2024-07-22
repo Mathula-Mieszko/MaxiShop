9095b6e752d24eb322406222b34cd41e4c217744
Mathula-Mieszko <mathula@mieszko.uk>
2024-07-22 12:54:02 +0100

    Folder structure for layer architecture done

---
+++ MaxiShop.Infrastructure/Migrations/20240722115151_BaseModelUpdatedMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaxiShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaseModelUpdatedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateOn",
+                table: "Categories",
+                newName: "CreatedOn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedOn",
+                table: "Categories",
+                newName: "CreateOn");
+        }
+    }
+}
